4bd4947860e10a8576aaca39d1efaf527fc9f25c
stareman1213 <stareman1213@gmail.com>
2026-08-17 16:08:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gachashokudo-v4';
+const CACHE = 'gachashokudo-v6';
 const CORE = ['./', './index.html', './support.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
